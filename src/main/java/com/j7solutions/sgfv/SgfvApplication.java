2faf275f239ab75299ae3aa8b74d4da3c7cbdb23
Juniorgtr1 <juniorgtr1@gmail.com>
2021-12-20 11:00:22 -0300

    Projeto inicializado, sem alteracoes

---
+++ src/main/java/com/j7solutions/sgfv/SgfvApplication.java
@@ -0,0 +1,13 @@
+package com.j7solutions.sgfv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j7solutions/sgfv/SgfvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j7solutions.sgfv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
